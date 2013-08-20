0b6b99b7567ab9e62fb84e0b835d806a9800413b
anna mendoza <anna@curvecommunications.biz>
2013-08-19 18:30:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhartlBookThirdApp::Application.config.secret_token = '3d6776011df0241d30c2556ad8080080bee64166e46c6b841b89ac611055d0abbe3b8bf518b14ba6a609eb00f4dd148529fc0992fe1f45df1946ffae0a253742'
